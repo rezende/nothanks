0f9fa94a1ad106816673580b981d35620a8032dc
Vinicius Rezende <vgrezende@gmail.com>
2019-11-27 17:50:12 -0300

    Minor display improvements

--- src/board.js
+++ src/board.js
@@ -13,16 +13,16 @@ export class IdRatherNotBoard extends React.Component {
     const { G, playerID, isActive, ctx } = this.props
     return (
       <div>
-        <p> Table: [{G.tableCard}] <span>{"o".repeat(G.tableTokens)} ({G.tableTokens})</span> </p>
-        <p> My tokens: <span>{"o".repeat(G.players[playerID].tokens)} ({G.players[playerID].tokens})</span> </p>
+        <p> <div style={{ 'display': 'flex', 'justify-content': 'center'}}><span style={{ 'font-weight': 'bold', 'font-size': '200%' }}>{G.tableCard}</span> <span>{"o".repeat(G.tableTokens)} ({G.tableTokens})</span> </div></p>
+        <p><button onClick={this.noThanks.bind(this)} disabled={!isActive || G.players[playerID].tokens < 1}>No Thanks</button> <button onClick={this.takeCard.bind(this)} disabled={!isActive}>Take Card</button></p>
         <p> My cards: [{G.players[playerID].cards.join('], [')}] </p>
+        <p> My tokens: <span>{"o".repeat(G.players[playerID].tokens)} ({G.players[playerID].tokens})</span> </p>
         <p> My score: [{G.players[playerID].publicScore}] </p>
         {ctx.gameover && 
           <div>
             <p>Final Score {ctx.gameover.scores[playerID].final}</p>
           </div>
         }
-        <p><button onClick={this.noThanks.bind(this)} disabled={!isActive || G.players[playerID].tokens < 1}>No Thanks</button> <button onClick={this.takeCard.bind(this)} disabled={!isActive}>Take Card</button></p>
       </div>
     )
   }
